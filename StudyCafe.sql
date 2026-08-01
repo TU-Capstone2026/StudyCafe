3597c3fd645d834087d964d65aaf2bb0b08089aa
Cyclaret <bespace02@gmail.com>
2026-08-01 15:52:04 +0900

    전체좌석, 좌석추가

--- StudyCafe.sql
+++ StudyCafe.sql
@@ -6,3 +6,11 @@ CREATE TABLE seats (
     CONSTRAINT UQ_seats_seat_number UNIQUE (seat_number)
 ) DEFAULT CHARSET=utf8mb4;
 
+-- 아래는 Oracle용
+CREATE TABLE seats (
+    seat_id     NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    seat_number VARCHAR2(10 CHAR) NOT NULL,
+    status      VARCHAR2(10 CHAR) DEFAULT '이용가능' NOT NULL,
+
+    CONSTRAINT UQ_seats_seat_number UNIQUE (seat_number)
+);
