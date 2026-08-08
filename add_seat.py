d6e1cb36c2c783efb2e97b85b4a614d41aca3613
Cyclaret <bespace02@gmail.com>
2026-08-08 12:16:10 +0900

    임시 push

--- add_seat.py
+++ add_seat.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_connection():
     return mysql.connector.connect(
         host="0.tcp.jp.ngrok.io",   # 지금 켜둔 ngrok 주소
-        port=14682,                  # 지금 켜둔 ngrok 포트
+        port=25818,                  # 지금 켜둔 ngrok 포트
         user="root",
         password="studycafe1234!",
         database="studycafe"
